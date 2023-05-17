7433007105aef73dab308dd31073c486eef43d24
raspberry-pi <raspberry-pi@kakao.com>
2023-05-17 05:31:20 +0000

    Implement Thread Pool

---
+++ Makefile
@@ -0,0 +1,52 @@
+CC = gcc-9
+CFLAGS := -Wall -Wextra -g
+INCLUDE = -Iinclude/
+
+SRC_DIR := ./src
+OBJ_DIR := ./obj
+BIN_DIR := ./bin
+INC_DIRS := -I./include/server -I./include/server -I./include/common
+
+$(shell mkdir -p $(OBJ_DIR)/common $(OBJ_DIR)/server $(OBJ_DIR)/command)
+$(shell mkdir -p $(BIN_DIR))
+
+# Source files
+COMMON_SRCS := $(wildcard $(SRC_DIR)/common/*.c)
+SERVER_SRCS := $(wildcard $(SRC_DIR)/server/*.c)
+COMMAND_SRCS := $(wildcard $(SRC_DIR)/command/*.c)
+
+# Object files
+COMMON_OBJS := $(patsubst $(SRC_DIR)/common/%.c,$(OBJ_DIR)/common/%.o,$(COMMON_SRCS))
+SERVER_OBJS := $(patsubst $(SRC_DIR)/server/%.c,$(OBJ_DIR)/server/%.o,$(SERVER_SRCS))
+COMMAND_OBJS := $(patsubst $(SRC_DIR)/command/%.c,$(OBJ_DIR)/command/%.o,$(COMMAND_SRCS))
+
+# Output binaries
+SERVER_TARGET := $(BIN_DIR)/server
+COMMAND_TARGET := $(BIN_DIR)/command
+
+# Default target
+all: $(SERVER_TARGET) $(COMMAND_TARGET)
+
+# Linking
+$(SERVER_TARGET): $(COMMON_OBJS) $(SERVER_OBJS)
+	$(CC) $(CFLAGS) $(INC_DIRS) $^ -o $@ -pthread
+
+$(COMMAND_TARGET): $(COMMON_OBJS) $(COMMAND_OBJS)
+	$(CC) $(CFLAGS) $(INC_DIRS) $^ -o $@ -pthread
+
+# Compilation
+$(OBJ_DIR)/common/%.o: $(SRC_DIR)/common/%.c
+	$(CC) $(CFLAGS) $(INC_DIRS) -c $< -o $@ -pthread
+
+$(OBJ_DIR)/server/%.o: $(SRC_DIR)/server/%.c
+	$(CC) $(CFLAGS) $(INC_DIRS) -c $< -o $@ -pthread
+
+$(OBJ_DIR)/command/%.o: $(SRC_DIR)/command/%.c
+	$(CC) $(CFLAGS) $(INC_DIRS) -c $< -o $@ -pthread
+
+# Clean
+clean:
+	rm -rf $(OBJ_DIR)/* $(BIN_DIR)/*
+
+# Phony targets
+.PHONY: all clean
---
+++ src/server/main.c
@@ -0,0 +1,133 @@
+
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h> // read(), write(), close()
+#include <pthread.h>
+#define MAX 80
+#define MAX_CONNECTOINS 5
+#define PORT 8080
+#define SA struct sockaddr
+pthread_mutex_t connection_mutex;
+pthread_cond_t connection_condition;
+int accepted_fd;
+
+void connection_handler(int connfd, char *request_buff, char *response_buff)
+{
+    while (1)
+    {
+        bzero(request_buff, MAX);
+
+        int bytes_size = recv(connfd, request_buff, MAX, 0);
+        if (bytes_size == 0)
+        {
+            printf("%d fd is disconnected.\n",connfd);
+            break;
+        }
+        memcpy(response_buff, request_buff, MAX);
+        write(connfd, response_buff, MAX);
+    }
+}
+void *connection_thread_handler()
+{
+    int connfd;
+    char request_buff[MAX];
+    char response_buff[MAX];
+
+    while (1)
+    {
+        pthread_mutex_lock(&connection_mutex);
+        while (accepted_fd == 0)
+        {
+            pthread_cond_wait(&connection_condition, &connection_mutex);
+        }
+        connfd = accepted_fd;
+        accepted_fd = 0;
+        pthread_mutex_unlock(&connection_mutex);
+        connection_handler(connfd, request_buff, response_buff);
+    }
+}
+
+// Driver function
+int main()
+{
+    int sockfd, connfd;
+    socklen_t len;
+    struct sockaddr_in servaddr, cli;
+    pthread_t thread_pool[MAX_CONNECTOINS];
+    memset(thread_pool, 0, sizeof(thread_pool));
+    pthread_mutex_init(&connection_mutex, NULL);
+    pthread_cond_init(&connection_condition, NULL);
+    // socket create and verification
+
+    for (int i = 0; i < MAX_CONNECTOINS; i++)
+    {
+        pthread_create(thread_pool + i, NULL, connection_thread_handler, NULL);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        printf("socket creation failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully created..\n");
+
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0)
+    {
+        printf("socket bind failed...\n");
+        exit(0);
+    }
+    else
+        printf("Socket successfully binded..\n");
+
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0)
+    {
+        printf("Listen failed...\n");
+        exit(0);
+    }
+    else
+        printf("Server listening..\n");
+    len = sizeof(cli);
+
+    // Accept the data packet from client and verification
+    while (1)
+    {
+        connfd = accept(sockfd, (SA *)&cli, &len);
+        if (connfd < 1)
+        {
+            printf("server accpet faild... \n");
+        }
+        pthread_mutex_lock(&connection_mutex);
+        accepted_fd = connfd;
+        pthread_mutex_unlock(&connection_mutex);
+        pthread_cond_signal(&connection_condition);
+    }
+
+    connfd = accept(sockfd, (SA *)&cli, &len);
+    printf("connfd: %x\n", connfd);
+    if (connfd < 0)
+    {
+        printf("server accept failed...\n");
+        exit(0);
+    }
+    else
+        printf("server accept the client...\n");
+
+    // After chatting close the socket
+    close(sockfd);
+    return 0;
+}
