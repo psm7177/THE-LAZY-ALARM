c34e0b087e0f9dd924a6134218eac2a3fe977608
raspberry-pi <raspberry-pi@kakao.com>
2023-06-01 15:00:57 +0000

    Implement make_request

---
+++ include/command/request.h
@@ -0,0 +1,8 @@
+#ifndef _REQUEST
+#define _REQUEST
+
+#include <protocol.h>
+#include <parser.h>
+
+request_t* make_request(arg_t *arg);
+#endif
---
+++ src/command/request.c
@@ -0,0 +1,38 @@
+#include <request.h>
+#include <stdlib.h>
+
+request_t* make_request(arg_t * arg){
+    request_t * req = calloc(1,sizeof(request_t));
+
+    char* cur = req->body;
+    switch (arg->method)
+    {
+    case CREATE:
+        cur += serialize_time(cur, arg->hours, arg->minutes);
+        break;
+    default:
+        break;
+    }
+
+    for(int i = 0; i < arg->num_options; i++){
+        option_t option = arg->options[i];
+        req->options[i] = option.type;
+        switch (option.type)
+        {
+        case OPTION_DIFFICULTY:
+        case OPTION_MUSIC:
+        case OPTION_REPEAT:
+        case OPTION_VOLUME:
+            cur += serialize_char(cur, option.value);
+            break;
+        case OPTION_TIME:
+            cur += serialize_time(cur, option.value[0], option.value[1]);
+            break;
+        default:
+            break;
+        }
+    }
+    req->num_options = arg->num_options;
+
+    return req;
+}
