7b19588728fb10e01c0f72b828ef742c8edc77f5
raspberry-pi <raspberry-pi@kakao.com>
2023-06-01 09:29:01 +0000

    Add parser.h

---
+++ include/command/parser.h
@@ -0,0 +1,30 @@
+#ifndef _PARSER
+#define _PARSER
+#include <stdint.h>
+#include <stdbool.h>
+
+#define COMMAND_CREATE 0
+
+typedef struct _arg arg_t;
+// alarm create hh:mm:ss [-d difficulty] [-m music] [-v volume] [-r the number of repeat]
+struct _arg
+{
+    uint8_t method;
+    uint8_t hours;
+    uint8_t minutes;
+    bool is_difficulty;
+    bool is_music;
+    bool is_volume;
+    bool is_repeat;
+    bool is_time;
+    uint8_t difficulty;
+    uint8_t music;
+    uint8_t volume;
+    uint8_t repeat;
+};
+
+uint8_t parse_method(char *str);
+void parse_create(arg_t *arg, int argc, char **argv);
+void parse_time(char *str, uint8_t *hours, uint8_t *minutes);
+void parse_arg(arg_t *arg, int argc, char **argv);
+#endif
