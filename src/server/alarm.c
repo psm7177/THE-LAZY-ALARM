1981b364fdeb5b785c4f1e718a4d4bdb16b72f6b
Moo-75 <131533724+Moo-75@users.noreply.github.com>
2023-06-04 00:12:42 +0900

    Update alarm.c
    signal_handler()랑 check_alarm()구현
    check_alarm() 스레드는 매 분마다 시간을 체크하면서 알람 리스트 계속 확인 후, 시간이 맞으면 alarm_sig_set에 signal을 push
    signal_handler()는 alarm_sig_set에 signal이 들어올 때까지 대기하다가 signal이 생기면 set에 push된 순서대로 처리
    
    현재는 (알람 켜기 + 미션주기), 알람끄기 두 가지 기능 밖에 없음

--- src/server/alarm.c
+++ src/server/alarm.c
@@ -1,7 +1,23 @@
 #include <alarm.h>
+#include <alarm-context.h>
 #include <mission.h>
 #include <music.h>
 #include <stdint.h>
+#include <unistd.h>
+int START_ALARM = 0;
+int STOP_ALARM = 1;
+
+typedef struct _alarm_sig {
+    list_t* sig_list;
+    list_t* sig_alarm_list;
+
+
+    pthread_mutex_t sig_mutex;
+    pthread_cond_t sig_cond;
+
+}alarm_sig_t;
+
+alarm_sig_t alarm_sig_set;
 
 alarm_t *create_alarm(uint8_t id, uint8_t hour, uint8_t minute) {
     alarm_t *alarm = malloc(sizeof(alarm_t));
@@ -14,86 +30,144 @@ alarm_t *create_alarm(uint8_t id, uint8_t hour, uint8_t minute) {
     // alarm->music = music;
     alarm->volume = VOLUME_MAX;
     // alarm->repeat_op = repeat_op;
-    alarm->active = 0;
+    // alarm->active = 0;
 
     return alarm;
 };
 
 void delete_alarm(alarm_t *alarm) {
+    pthread_mutex_lock(&alarm_mutex);
+    for (size_t i = 0; i < alarm_list->num; i++)
+    {
+        alarm_t *tmp = (alarm_t *) access_item(alarm_list, i);
+        if (tmp->id == alarm->id) delete_item(alarm_list, i);
+    }
     free(alarm);
+    pthread_mutex_unlock(&alarm_mutex);
 }
 
-void turn_on(alarm_t *alarm) {
-    alarm->active = 1;
-}
-void turn_off(alarm_t *alarm) {
-    alarm->active = 0;
-}
+// void turn_on(alarm_t *alarm) {
+//     alarm->active = 1;
+// }
+// void turn_off(alarm_t *alarm) {
+//     alarm->active = 0;
+// }
+
 void syncronize() {
     time_t rawtime;
     struct tm* timeinfo;
     time(&rawtime);
     timeinfo = localtime(&rawtime);
+    printf("the time is %i : %i : %i\n", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
 
     while (1)
     {
         time(&rawtime);
         timeinfo = localtime(&rawtime);
-        if (timeinfo->tm_sec != 0 && timeinfo->tm_sec != 1) wait(1); else break;
+        if (timeinfo->tm_sec != 0 && timeinfo->tm_sec != 1) sleep(1); else break;
     }
 }
 
 time_t time_check(list_t *alarm_list) {
     time_t curr;
     struct tm* timeinfo;
-    time(&curr);
-    timeinfo = localtime(&curr);
 
+    pthread_mutex_lock(&alarm_mutex);
     for (size_t i = 0; i < alarm_list->num; i++)
     {
+        time(&curr);
+        timeinfo = localtime(&curr);
+
         alarm_t *alarm = access_item(alarm_list, i);
         if (alarm->target_time.tm_hour == timeinfo->tm_hour && alarm->target_time.tm_min == timeinfo->tm_min)
         {
-            start_alarm(alarm);
+            pthread_mutex_lock(&alarm_sig_set.sig_mutex);
+
+            push_item(alarm_sig_set.sig_list, &START_ALARM);
+            push_item(alarm_sig_set.sig_alarm_list, alarm);
+            pthread_cond_signal(&alarm_sig_set.sig_cond);
+
+            pthread_mutex_unlock(&alarm_sig_set.sig_mutex);
         }
     }
-
+    pthread_mutex_unlock(&alarm_mutex);
+    
     syncronize();
     return curr;
 }
 
-void start_alarm(alarm_t *alarm) {
-    play_music(alarm->music);
-
-    for (size_t i = 0; i < alarm->mission_list->num; i++)
-    {
-        mission_t *mission = (mission_t *) access_item(alarm->mission_list, i);
-        exe_mission(mission);
-    }
-    
-    turn_off_music();
-}
-
-int test_alarm() {
-    time_t curr;
-    struct tm* target_time = localtime(&curr);
-    list_t *mission_list = create_list(sizeof(mission_t));
-    alarm_t *a = create_alarm(123, target_time->tm_hour, target_time->tm_min);
-    alarm_t *b = create_alarm(121, target_time->tm_hour, target_time->tm_min);
-
-    list_t *alarm_list = create_list(sizeof(alarm_t));
-    push_item(alarm_list, a);
-    push_item(alarm_list, b);
-
-    turn_on(a);
-    turn_on(b);
-
+void *check_alarm(void *arg) {
     syncronize();
-    
     while (1)
     {
         time_check(alarm_list);
-        wait(60);
+        sleep(60);
     }
+    return 0;
+}
+
+void *signal_handler(void *arg){
+    while (1)
+    {
+        pthread_mutex_lock(&alarm_sig_set.sig_mutex);
+        if (alarm_sig_set.sig_list->num == 0)
+        {
+            pthread_cond_signal(&alarm_sig_set.sig_cond);
+        } else {
+            int* single_sign = access_item(alarm_sig_set.sig_list, 0);
+            alarm_t* target = access_item(alarm_sig_set.sig_alarm_list, 0);
+            switch (*single_sign)
+            {
+            case 0: //알람 켜기 + 미션주기
+                play_music(target->music);
+
+                for (size_t i = 0; i < target->mission_list->num; i++)
+                {
+                    mission_t *mission = (mission_t *) access_item(target->mission_list, i);
+                    exe_mission(mission);
+                }
+                delete_item(alarm_sig_set.sig_list, 0);
+                delete_item(alarm_sig_set.sig_alarm_list, 0);
+                push_item(alarm_sig_set.sig_list, &STOP_ALARM);
+                push_item(alarm_sig_set.sig_alarm_list, target);
+                pthread_cond_signal(&alarm_sig_set.sig_cond);
+                break;
+            case 1: //알람 끄기
+                turn_off_music();
+                delete_item(alarm_sig_set.sig_list, 0);
+                delete_item(alarm_sig_set.sig_alarm_list, 0);
+                break;
+                    
+            //TODO : implement other cases ex) setting alarm
+
+            default:
+                break;
+            }
+        }
+        pthread_mutex_unlock(&alarm_sig_set.sig_mutex);
+    }
+    return 0;
+}
+
+int test_() {
+    alarm_t *A = create_alarm(123, 23, 57);
+    A->mission_list = create_list(sizeof(char*));
+    alarm_list = create_list(sizeof(alarm_t));
+    alarm_sig_set.sig_alarm_list = create_list(sizeof(alarm_t));
+    alarm_sig_set.sig_list = create_list(sizeof(int));
+    mission_t* mission;
+    mission->name = "press buttons";
+    mission->difficulty = 1;
+    mission->success = 0;
+    push_item(A->mission_list, mission);
+    push_item(alarm_list, A);
+
+    pthread_t checking_alarm, handling;
+
+    pthread_create(&checking_alarm, 0, &check_alarm, 0);
+    pthread_create(&handling, 0, &signal_handler, 0);
+
+    pthread_join(checking_alarm, 0);
+    pthread_join(handling, 0);
     return 0;
 }
