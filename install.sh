09878ddf3090f955cf8a943fe62fe324fff9e76b
Moo-75 <cotlgjs@dgist.ac.kr>
2023-06-08 04:07:24 +0900

    add install pigpio module

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!
 sudo apt-get install gcc-9
-sudo apt install libpcsclite-dev pcscd libpcsclite1 pcsc-tools
+sudo apt-get install libpcsclite-dev pcscd libpcsclite1 pcsc-tools
+sudo apt-get install pigpio
